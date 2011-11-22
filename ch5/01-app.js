4eaf7cba5b3ad3a39b60b1166cb47a866e8098f8
Adriaan de Jonge <adriaandejonge@gmail.com>
2011-11-22 14:56:53 +0100

    Remove tabs

--- ch5/01-app.js
+++ ch5/01-app.js
@@ -3,10 +3,10 @@ var http = require('http'),
     fs = require('fs');
 http.createServer(function (req, res) {
   var reqData = {
-		url: url.parse(req.url, true),
-		method: req.method,
-		headers: req.headers },
-  	  path = reqData.url.pathname;
+    url: url.parse(req.url, true),
+    method: req.method,
+    headers: req.headers },
+      path = reqData.url.pathname;
 
   if(path.match(/^\/[0-9a-z\-]+\.(html)|(json)|(xml)$/))
     fs.readFile('.' + path, function (err, data) {
@@ -14,12 +14,12 @@ http.createServer(function (req, res) {
         res.writeHead(404, {'Content-Type': 'text/plain'});
         res.end('not found');
       }
-	  else {
-	   if(path.split('.')[1] == 'html')
-  	     res.writeHead(200, {'Content-Type': 'text/html'});
-	   else if(path.split('.')[1] == 'xml')
-  	     res.writeHead(200, {'Content-Type': 'application/xml'});
-	   else 
+    else {
+     if(path.split('.')[1] == 'html')
+         res.writeHead(200, {'Content-Type': 'text/html'});
+     else if(path.split('.')[1] == 'xml')
+         res.writeHead(200, {'Content-Type': 'application/xml'});
+     else 
          res.writeHead(200, {'Content-Type': 'application/json'});
        res.end(data);
       }   
@@ -29,24 +29,24 @@ http.createServer(function (req, res) {
     res.end(JSON.stringify(reqData));
   }
   else if(path == '/sleep') {
-	var endTime = new Date().getTime() + 2000;
-	while (new Date().getTime() < endTime);
-	res.writeHead(500, {'Content-Type': 'text/plain'});
+  var endTime = new Date().getTime() + 2000;
+  while (new Date().getTime() < endTime);
+  res.writeHead(500, {'Content-Type': 'text/plain'});
     res.end('slow response');
   }
   else if(path == '/validate') {
-	var keys = [];
-	for(var key in reqData.url.query) {
-		if(reqData.url.query[key] == '')
-			keys.push(key);
-	}
-	res.writeHead(200, {'Content-Type': 'application/json'});
+  var keys = [];
+  for(var key in reqData.url.query) {
+    if(reqData.url.query[key] == '')
+      keys.push(key);
+  }
+  res.writeHead(200, {'Content-Type': 'application/json'});
     res.end(JSON.stringify(keys));
   }
   else if(path == '/redirect') {
-	res.writeHead(302, {
-	  'Location': '/test-values.json' });
-    res.end();	
+  res.writeHead(302, {
+    'Location': '/test-values.json' });
+    res.end();  
   }
   else if(path == '/fail\-on\-purpose') {
     res.writeHead(500, {'Content-Type': 'text/plain'});
